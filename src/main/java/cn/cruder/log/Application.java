3dc054732da693c5803e1d2c2f0219757d3a88e3
DouShaoxun <dousx_cn@163.com>
2022-03-11 17:56:28 +0800

    log4j2

--- src/main/java/cn/cruder/log/Application.java
+++ src/main/java/cn/cruder/log/Application.java
@@ -2,11 +2,13 @@ package cn.cruder.log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author dousx
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/cn/cruder/log/task/TestTask.java
@@ -0,0 +1,27 @@
+package cn.cruder.log.task;
+
+import cn.cruder.log.service.TestLogService;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: cruder
+ * @Date: 2022/03/11/17:52
+ */
+@Component
+@AllArgsConstructor
+public class TestTask {
+    private final TestLogService testLogService;
+
+
+    @Scheduled(fixedDelay = 5000)
+    public void task() {
+        //测试日志压缩 循环次数 根据需要调整
+        for (int i = 0; i < 10; i++) {
+            testLogService.logTest();
+        }
+
+    }
+
+}
